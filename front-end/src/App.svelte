03a265f7d66e41e6dff6f7d0cda1986ee9a7be6a
parkernilson <parker.todd.nilson@gmail.com>
2020-12-10 03:05:33 -0700

    set up frameworks

---
+++ front-end/src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="typescript">
+	import ContentColumn from './ui/ContentColumn.svelte';
+</script>
+
+<style>
+	:global(body) {
+		padding: 0;
+		font-family: Montserrat, sans-serif;
+	}
+	:root {
+		--card-border-radius: 42px;
+		--master-side-margins: 32px;
+	}
+</style>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;1,700&display=swap" rel="stylesheet">
+</svelte:head>
+
+<ContentColumn>
+	<h1>Gene Sequence Comparisons</h1>
+</ContentColumn>
---
+++ front-end/src/ui/ContentColumn.svelte
@@ -0,0 +1,13 @@
+<style>
+    .container {
+        margin: auto;
+        max-width: 960px;
+        padding-left: var(--master-side-margins);
+        padding-right: var(--master-side-margins);
+        flex: 1;
+    }
+</style>
+
+<div class="container">
+    <slot></slot>
+</div>
